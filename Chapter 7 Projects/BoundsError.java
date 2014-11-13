666993da899d083a3d22022a5ce93e4ffd7770d2
Resilienc3 <jsmitty1414@gmail.com>
2014-11-13 12:21:28 -0600

    Chapter Practice

---
+++ Chapter 7 Projects/BoundsError.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class BoundsError here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BoundsError
+{
+   public static void main(String[] args)
+   {
+       int[] scores = new int[10];
+       for(int i = 0; i< scores.length; i++)
+       {
+           scores[i] = i+1;
+       }
+   }
+}
